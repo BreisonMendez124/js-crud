60fcec29962922a89c95178049529cee5dc8645b
BreisonMendez124 <56001833+BreisonMendez124@users.noreply.github.com>
2021-11-05 08:56:57 -0500

    Add files via upload

--- AJAX/src/js/login.js
+++ AJAX/src/js/login.js
@@ -1,57 +1,32 @@
 
+const login = () =>{
+  
+  var name = $('#name').val()
+  var password = $('#password').val()
 
-let auth0 = null
+  console.log(name, password)
+    $.ajax( {
+        url         : 'http://155.248.231.81:8080/api/Client/all',
+        type        : 'GET',
+        dataType    : 'json',
+        success     : function(json){
+                        $('#tabla').empty();
+                        for(let i = 0; i < json.length; i++){
+                          console.log(name,json[i].name, password,json[i].password)
+                          if(name == json[i].name && password == json[i].password ){
+                            alert("usuario correcto")
+                            
+                            
+                          }else{
+                            alert("credenciales incorrectas")
+                          }
+                        }
+                      },
+        error       :function(xhr,status){
+                        console.error(`no se puede conectar al servicio :( ${xhr}`);
+                    }
 
-window.onload = async () => {
-  await configureClient()
-  await processLoginState()
-  updateUI()
-}
-
-const configureClient = async () => {
-  auth0 = await createAuth0Client({
-    domain: "dev-t7jqyrxq.us.auth0.com",
-    client_id: "rfMuKfYlCdAgdEHlA9zbtZY1ZnpM0Nne",
-  })
-}
-
-const processLoginState = async () => {
-  // Check code and state parameters
-  const query = window.location.search
-  if (query.includes("code=") && query.includes("state=")) {
-    // Process the login state
-    await auth0.handleRedirectCallback()
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, window.location.pathname)
-  }
-}
-
-const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated()
-  document.getElementById("btn-logout").disabled = !isAuthenticated
-  document.getElementById("btn-login").disabled = isAuthenticated
-  // NEW - add logic to show/hide gated content after authentication
-  if (isAuthenticated) {
-    document.getElementById("gated-content").classList.remove("hidden")
-    document.getElementById(
-      "ipt-access-token"
-    ).innerHTML = await auth0.getTokenSilently()
-    document.getElementById("ipt-user-profile").innerHTML = JSON.stringify(
-      await auth0.getUser()
-    )
-  } else {
-    document.getElementById("gated-content").classList.add("hidden")
-  }
-}
-
-const login = async () => {
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.href,
-  })
-}
-
-const logout = () => {
-  auth0.logout({
-    returnTo: window.location.href,
-  })
+        
+    })
+    location.href="http://127.0.0.1:5500/dates/disfraz.html"
 }
